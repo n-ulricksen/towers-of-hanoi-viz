e8e901185b964f92d248f1855c77a4f1c7962168
Nicholas Ulricksen <n.ulricksen@gmail.com>
2020-12-08 07:07:15 -0800

    implement towers of hanoi w/ visualization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import Tower from "./Tower.js";
+import "./App.css";
+
+const INITIAL_START_TOWER = [7, 6, 5, 4, 3, 2, 1];
+
+let timers = [];
 
 function App() {
+  const [tower0, setTower0] = useState(INITIAL_START_TOWER);
+  const [tower1, setTower1] = useState([]);
+  const [tower2, setTower2] = useState([]);
+
+  const towers = [tower0, tower1, tower2];
+
+  function solveHanoi(n) {
+    let moves = [];
+    const _solveHanoi = (n, from, to, spare) => {
+      if (n === 1) {
+        const move = [from, to];
+        moves.push(move);
+      } else {
+        _solveHanoi(n - 1, from, spare, to);
+        _solveHanoi(1, from, to, spare);
+        _solveHanoi(n - 1, spare, to, from);
+      }
+    };
+    _solveHanoi(n, 0, 2, 1);
+
+    // Timer is kept track of in order to allow user to cancel visualization
+    // at any time by clearing all timers and resetting to original state.
+    let timer;
+    moves.forEach((move, i) => {
+      timer = setTimeout(() => moveRing(move[0], move[1]), (1 + i) * 200);
+      timers.push(timer);
+    });
+  }
+
+  // Move a ring from one tower to another. The last element of the
+  // tower's array represents the top ring on the tower.
+  function moveRing(from, to) {
+    if (from < 0 || from > 2 || to < 0 || to > 2 || from === to) {
+      throw new Error(`Invalid rings: ${from}, ${to}`);
+    }
+
+    const movingRing = towers[from][towers[from].length - 1];
+    towers[from] = towers[from].slice(0, towers[from].length - 1);
+    towers[to].push(movingRing);
+
+    updateTowersState();
+  }
+
+  function updateTowersState() {
+    setTower0(towers[0]);
+    setTower1(towers[1]);
+    setTower2(towers[2]);
+  }
+
+  function reset() {
+    for (let i = 0; i < timers.length; i++) {
+      clearTimeout(timers[i]);
+    }
+
+    towers[0] = INITIAL_START_TOWER;
+    towers[1] = [];
+    towers[2] = [];
+    updateTowersState();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tower rings={tower0} />
+      <Tower rings={tower1} />
+      <Tower rings={tower2} />
+      <button onClick={() => solveHanoi(INITIAL_START_TOWER.length)}>
+        Solve!
+      </button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
---
+++ src/Ring.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Ring({ number }) {
+  const MIN_WIDTH = 80;
+  const INC_WIDTH = 15;
+
+  const width = MIN_WIDTH + INC_WIDTH * number;
+
+  return (
+    <div className="Ring" style={{ width }}>
+      {number}
+    </div>
+  );
+}
